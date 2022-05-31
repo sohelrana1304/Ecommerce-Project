cdc70892df4bced1ea75fb48457dbda0ff3eb1cd
karthi909 <karthikeyareddy440@gmail.com>
2022-05-31 16:47:50 +0530

    done

---
+++ src/controller/orderController.js
@@ -0,0 +1,11 @@
+const orderModel = require('../models/orderModel')
+const validation = require('../validations/validation')
+
+
+
+
+
+
+
+
+module.exports = { createOrder, updateOrder }
---
+++ src/models/orderModel.js
@@ -0,0 +1,34 @@
+const mongoose=require("mongoose")
+
+const ObjectId=mongoose.Types.ObjectId
+
+const oderSchema = new mongoose.Schema({
+
+
+
+    userId: {type:ObjectId, refs:"UserData", required:true},
+
+    items: [{
+      productId: {type:ObjectId, refs:"Product" , required:true},
+      quantity: {type:Number, required:true, min:1}
+    }],
+
+    totalPrice: {type:Number, required:true },
+
+    totalItems: {type:Number, required:true},
+
+    totalQuantity: {type:Number, required:true},
+
+    cancellable: {type:Boolean, default: true},
+
+    status: {type:String, default: 'pending', enum:["pending", "completed", "canclled"]},
+
+    deletedAt: {type:Date}, 
+
+    isDeleted: {type:Boolean, default: false},
+    
+
+
+})
+
+module.exports=mongoose.model("Order",oderSchema)
